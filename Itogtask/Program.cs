ab7f6aa8d98e5acd9c374f5ceef4813d77ce66a2
Aleksandr <makasar83@mail.ru>
2022-10-28 13:38:21 +0300

    написал итоговое задание

---
+++ Itogtask/Program.cs
@@ -0,0 +1,32 @@
+﻿string[] array1 = new string[5];
+ 
+ for (int i = 0; i < 5; i++)
+{
+    array1[i] = Console.ReadLine();
+}
+
+
+string[] array2 = new string[array1.Length];
+void FinalArray(string[] array1, string[] array2)
+{
+    int n = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[n] = array1[i];
+        n++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+FinalArray(array1, array2);
+PrintArray(array1);
+PrintArray(array2);
